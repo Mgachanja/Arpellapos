2285d822926c3a1dac39c6f657e83b0b0a5fa8ee
Mgachanja <markgachanja1@gmail.com>
2025-10-11 15:20:08 +0300

    corrected the search input

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { store } from "../redux/store";
+import { store } from "./src/redux/store";
 const { app, BrowserWindow, ipcMain, dialog } = require('electron');
 const path = require('path');
 const fs = require('fs');
